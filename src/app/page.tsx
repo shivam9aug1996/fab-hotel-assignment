fcda0ea4d3d58880a113c49610d63d7d14295197
shivam-appboxer <shivam@appboxer.com.au>
2024-07-16 00:04:29 +0530

    ui fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HomePage = () => {
   return (
-    <div className="relative p-16 rounded-lg bg-slate-300 text-center px-32">
+    <div className="relative p-16 rounded-lg bg-white text-center px-32">
       <h1 className="text-3xl font-bold text-black font-serif">Welcome to</h1>
       <span className="inline-flex items-center justify-center text-gray-800 bg-yellow-400 rounded-full w-10 h-10 mr-1">
         fab
